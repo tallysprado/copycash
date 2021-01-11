e7b0659022e322b7d3c85b2f3e13892be2f185cf
tallys <tallys.prado@gmail.com>
2021-01-11 14:54:37 -0300

    story

--- components/Sobre/index.js
+++ components/Sobre/index.js
@@ -5,7 +5,13 @@ const Index = () => {
         <div id='sobre'>        
             <div className='background'/> 
             <div className='story'>
-                
+                <h3>Ladismérico Matheus</h3>
+                <p>
+                    É esse cara aqui quem vai botar pra quebrar <del>a banca</del> na tua conta!
+                    Chupando a picona da sua progenitora ele vai fumar a nicotina dele e 
+                    operar no forex pra depois pensar em fumar uma cannabis.
+                </p>
+            
             </div>
         </div>
 
