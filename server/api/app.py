ff9cf6975f06a04d73c917a6700a06aa8f2de519
tallys <tallys.prado@gmail.com>
2021-01-24 15:21:25 -0300

    cors origins

--- server/api/app.py
+++ server/api/app.py
@@ -17,7 +17,7 @@ app.config['JWT_REFRESH_LIFESPAN'] = {'days':30}
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 
-cors = CORS(app, resouces={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "http://localhost:5000"}})
 guard = flask_praetorian.Praetorian()
 cors.init_app(app)
 db = SQLAlchemy()
