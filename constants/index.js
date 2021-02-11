439a0416fd9e52e02d32c08c546d32134c7a2571
tallys <tallys.prado@gmail.com>
2021-02-11 01:12:49 -0300

    http back

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 //export const BASE_URL = "http://localhost:5000" //flask
-export const BASE_URL = "https://35.199.92.216:8080";
+export const BASE_URL = "http://35.199.92.216:8080";
 export const BUY_URL = BASE_URL + "/buy";
 export const PAIR_URL = BASE_URL + "/paridades";
 export const SAVE_CONFIGS = BASE_URL + "/config";
